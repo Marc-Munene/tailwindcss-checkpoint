dc5541702dca460bcb94002af57805c030c2352b
Marc Munene <marcmunene104@gmail.com>
2024-11-19 11:14:52 +0300

    checkpoint commit

---
+++ blog - portfolio/new-blog/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        bluish : '#f5d7c3'
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
+
